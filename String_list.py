9b963b501a7e09ad34a05d5f2f9cfce9558abe6c
BillDoors23 <BillDoors23@gmail.com>
2016-02-16 20:56:02 +0800

    Update String_list.py

--- String_list.py
+++ String_list.py
@@ -4,15 +4,15 @@ var = []
 for i in input:
     var.append(i)
 
-if var == var[::-1]:
-    print(input.upper() + " is palindrome!\n")
+if var == var[::-1]:                                    #"".join() make[a, b, c] to abc
+    print(input.upper() + " is palindrome!\n")                          
     print("\tINPUT")
-    print(var)
+    print("".join(var))
     print("\tREVERSED")
-    print(var[::-1])
+    print("".join(var[::-1]))
 else:
     print(input.upper() + " is not a palindrome!\n")
     print("\tINPUT")
-    print(var)
+    print("".join(var))
     print("\tREVERSED")
-    print(var[::-1])
+    print("".join(var[::-1]))
